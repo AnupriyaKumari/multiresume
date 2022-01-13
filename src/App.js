7bbe94a0ae13ecc9d9cf21837b2f75b7cba360d5
jaatboys <anmolrathi460@gmail.com>
2022-01-13 23:35:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,88 @@
+
+import Im from './MAHA KALL.jpg'
+
+function App() {
+
+
+  return (
+    <div class="flex items-center justify-center mt-5">
+
+	<div class="bg-blue-900 w-1/8 p-10 h-auto">
+		<div class="flex items-center justify-center mb-10">
+			<img src={Im} alt=""
+				class="rounded-full w-32 border-2 border-gray-300"/>
+		</div>
+		<h1 class="text-gray-400 uppercase tracking-widest text-lg font-bold">Contact</h1>
+		<hr class="w-1/6 mb-5"/>
+		<h1 class="text-gray-400 text-sm">leads4Needs@gmail.com</h1>
+		<h1 class="text-gray-400 text-sm">+12 456 455 7885</h1>
+		<h1 class="text-gray-400 text-sm">India</h1>
+		<h1 class="text-gray-400 text-sm">www.leads4Needs.com</h1>
+		<hr class="my-5"/>
+		<h1 class="text-gray-400 mt-2 uppercase tracking-widest text-lg font-bold">EDUCATION</h1>
+		<hr class="w-1/6 mb-5"/>
+		<h1 class="text-gray-400 text-sm uppercase font-semibold tracking-wider">Master of Science</h1>
+		<h1 class="text-gray-500 text-sm"><span class="italic">University Name</span>, <span
+				class="text-gray-400 font-semibold italic">New York</span> / 2012-2013</h1>
+		<br/>
+		<h1 class="text-gray-400 text-sm uppercase font-semibold tracking-wider">Bachelor of Arts</h1>
+		<h1 class="text-gray-500 text-sm"><span class="italic">College Name Here</span>, <span
+				class="text-gray-400 font-semibold italic">London</span> / 2011-2012</h1>
+		<hr class="my-5"/>
+		<h1 class="text-gray-400 mt-2 uppercase tracking-widest text-lg font-bold">SKILLS</h1>
+		<hr class="w-1/6 mb-5"/>
+		<ul>
+			<li class="text-gray-500 text-sm list-disc ml-4">Adobe Photoshop</li>
+			<li class="text-gray-500 text-sm list-disc ml-4">Adobe Illustrator</li>
+			<li class="text-gray-500 text-sm list-disc ml-4">Adobe After Effects</li>
+			<li class="text-gray-500 text-sm list-disc ml-4">Microsoft Power Point</li>
+			<li class="text-gray-500 text-sm list-disc ml-4">Microsoft Word</li>
+			<li class="text-gray-500 text-sm list-disc ml-4 mb-1">Adobe InDesign</li>
+		</ul>
+	</div>
+
+
+
+
+	<div class="bg-white w-7/12 p-10 h-auto">
+    <h1 className='font-bold' >TITLE</h1>
+		<p class="text-gray-500">A resourceful individual with a proven track record in implementing successful
+			marketing strategies, boosting organic traffic, and improving search rankings seeks a position of Marketing
+			Associate at ABC company to maximize brand awareness and revenue through integrated marketing
+			communications.</p>
+		<h1 class="font-semibold uppercase tracking-wider my-6 text-gray-500">Experience</h1>
+		<h1 class="text-gray-600 font-semibold">FullStack Developer (2018 - Present)</h1>
+		<ul class="list-disc ml-5 text-gray-500">
+			<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+			<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+			<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+		</ul>
+		<h1 class="text-gray-600 font-semibold mt-3">Sr Developer (2016 - 2-18)</h1>
+		<ul class="list-disc ml-5 text-gray-500">
+			<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+			<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+			<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+		</ul>
+		<h1 class="text-gray-600 font-semibold mt-3">Sr Developer (2016 - 2-18)</h1>
+		<ul class="list-disc ml-5 text-gray-500">
+			<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+			<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+			<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+		</ul>
+		<h1 class="text-gray-600 font-semibold mt-3">Sr Developer (2016 - 2-18)</h1>
+		<ul class="list-disc ml-5 text-gray-500">
+			<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+			<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+			<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+		</ul>
+		<h1 class="text-gray-600 font-semibold mt-3">Sr Developer (2016 - 2-18)</h1>
+		<ul class="list-disc ml-5 text-gray-500">
+			<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+			<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+		</ul>
+	</div>
+</div>
+  )
+}
+
+export default App;
